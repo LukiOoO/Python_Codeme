fd111883fc138f7426e8422d94a004ff96ec4911
LukiOoO <leszczynskilukasz2003@gmail.com>
2022-02-23 19:39:10 +0100

    upgrade

---
+++ 01_intro/new_skill.py
@@ -0,0 +1,7 @@
+learnig_h = 75
+houers_per_week = int(input("Ile godzin na nauke [h]: "))
+
+weeks = learnig_h / houers_per_week
+
+print("liczba tygodni nauki ", weeks)
+print(f'liczba tygodni nauki {weeks} tygodni')
---
+++ 01_intro/weeks.py
@@ -0,0 +1,6 @@
+weeks = 7 * 7 * 24 * 60
+print(" 7 tygodni to w minutach = ", weeks)
+
+
+
+
