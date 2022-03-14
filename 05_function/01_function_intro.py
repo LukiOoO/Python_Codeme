8933dde9ecc28f4000cfe033fde7bdb51850c1c5
Łukasz <100160153+LukiOoO@users.noreply.github.com>
2022-03-14 14:39:19 +0100

    Add files via upload

---
+++ 05_function/01_function_intro.py
@@ -0,0 +1,25 @@
+# 1▹ Skorzystaj ze swojego kodu bmi.py. Rozbij liczenie bmi na funkcję obliczającą bmi na podstawie danych użytkownika oraz zwracającą odpowiednią wartość (niedowaga, waga normalna, nadwaga, otyłość) w zależności od otrzymanego parametru.
+
+
+# def circle_area(r):
+#     pi = 3.14
+#     return pi * (r **2)
+#
+#
+# area = circle_area()
+# print("Pole to: ",area)
+
+#
+# def circle_area(r):
+#     pi = 3.14
+#     print("Pole to: ",pi * (r**2))
+# radius = float(input("Podaj promien: "))
+# circle_area(radius)
+
+
+def circle_area():
+    r = float(input("Podaj promien: "))
+    pi = 3.14
+    print("Pole to: ",pi * (r**2))
+    return
+circle_area()
