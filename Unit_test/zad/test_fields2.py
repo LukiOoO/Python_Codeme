f4fef4bac963aa866a3930446ef1f91dbbbc4cab
Łukasz <100160153+LukiOoO@users.noreply.github.com>
2022-04-27 16:35:35 +0200

    Add files via upload

---
+++ Unit_test/zad/test_fields2.py
@@ -0,0 +1,23 @@
+
+import unittest
+from area import rectangle, triangle
+
+
+class FieldsTestCase(unittest.TestCase):
+    def setUp(self):
+        self.a = 10
+        self.b = 50
+
+    def test_rectangle_with_correct_values(self):
+        self.assertEqual(rectangle(self.a, self.b), 500)
+
+    def test_triangle_with_correct_values(self):
+        self.assertEqual(triangle(self.a, self.b), 250)
+
+    def tearDown(self):
+        del self.a
+        del self.b
+
+
+if __name__ == '__main__':
+    unittest.main()
